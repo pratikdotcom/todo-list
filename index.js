3f528ff5ea04cd44628f44ee026e89a15762c690
pratikdotcom <shindepratik624@gmail.com>
2024-07-03 15:54:27 +0530

    Commit 2 ( Added Database )

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 const {createTodo, updateTodo} = require("./types");
+const {createTodoDB, updateTodoDB} = require("../db");
 
 app.post("/todo", function(req, res){
     const createPayload = req.body;
-    const parsePayload = createTodo
+    const parsePayload = createTodo.safeParse(createPayload);
+    if(!parsePayload.success){
+        res.status(411).json({
+            msg: "You sent the wrong inputs"
+        })
+        return;
+    }
 });
 
 app.get("/todos", function(req, res){
@@ -13,7 +20,14 @@ app.get("/todos", function(req, res){
 });
 
 app.put("/completed", function(req, res){
-
+    const updatePayload = req.body;
+    const parsedUpdatePayload = updateTodo.safeParse(updatePayload);
+    if(!parsedUpdatePayload.success){
+        res.status(411).json({
+            msg: "You sent the wrong inputs"
+        })
+        return;
+    }
 });
 
 
