a2877686128c081818c444149739e0d9cf2c7b62
pratikdotcom <shindepratik624@gmail.com>
2024-07-03 17:59:24 +0530

    Complete ( Almost )

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { CreateTodo } from './components/CreateTodo'
+import { Todos } from './components/Todos'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([]);
+  fetch("https://localhost:3000/todos")
+  .then(async function(res){
+    const json = await res.json();
+    setTodos(json.todos);
+  })
+
 
   return (
     <div>
-      Hi there
+     <CreateTodo  />
+     <Todos todos={todos}/>
     </div>
   )
 }
