078d42cd869f0565e785c7fc49a4aacd5bce811d
pratikdotcom <shindepratik624@gmail.com>
2024-07-03 16:08:44 +0530

    Commit 3 ( Backend Done )

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 const {createTodo, updateTodo} = require("./types");
-const {createTodoDB, updateTodoDB} = require("./database");
+const {todo} = require("./database");
 
-app.post("/todo", function(req, res){
+app.post("/todo", async function(req, res){
     const createPayload = req.body;
     const parsePayload = createTodo.safeParse(createPayload);
     if(!parsePayload.success){
@@ -13,13 +13,26 @@ app.post("/todo", function(req, res){
         })
         return;
     }
+    await todo.create({
+        title: createPayload.title,
+        description: createPayload.description,
+        completed: false
+    })
+
+    res.json({
+        msg: "Todo created"
+    })
 });
 
-app.get("/todos", function(req, res){
-
+app.get("/todos", async function(req, res){
+   const todos = await todo.find(); // needs to await for todos to come 
+   console.log(todos) // if it isn't await it will give back a promise
+   res.json({
+    todos
+   })
 });
 
-app.put("/completed", function(req, res){
+app.put("/completed", async function(req, res){
     const updatePayload = req.body;
     const parsedUpdatePayload = updateTodo.safeParse(updatePayload);
     if(!parsedUpdatePayload.success){
@@ -28,10 +41,15 @@ app.put("/completed", function(req, res){
         })
         return;
     }
-
-
+  await todo.update({
+    _id: req.body.id
+  },{
+    completed: true
+  })
+  res.json({
+    msg: "Todo marked as completed!"
+  })
 });
-
 
 app.listen(3000, function(){
     console.log("Server is running on the port 3000!")
